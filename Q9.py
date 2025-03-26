61166d5da1fb6aff44ebe1c0f18f598f947602e7
Ahana Srinath <ahanasrinathmurthy@gmail.com>
2025-03-26 08:46:33 +0530

    Create Q9.py

---
+++ Q9.py
@@ -0,0 +1,20 @@
+#Palindrome Number
+#return true if x is a palindrome, and false otherwise.
+
+class Solution(object):
+    def isPalindrome(self, x):
+       if x<0:
+            return False
+        
+        temp = x
+        pal = 0
+
+        while x!=0:
+            rem = x%10
+            pal = pal*10 + rem
+            x = x//10
+        
+        if temp == pal:
+            return True 
+        else:
+            return False
